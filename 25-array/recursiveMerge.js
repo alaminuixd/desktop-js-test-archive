08bc6a443c4aa388cb971b855c1c3c4876970061
alamin khan <alaminuixd@gmail.com>
2025-03-23 02:49:24 +0600

    recursive merge array function

--- 25-array/recursiveMerge.js
+++ 25-array/recursiveMerge.js
@@ -1,16 +1,16 @@
 const arr = [
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
+  [1, 2],
+  [3, 4],
 ];
-// recursive function
-function merge(arr, merged = [], index = 0) {
-  if (index >= arr.length) return merged;
-  if (Array.isArray(arr[index])) {
-    merge(arr[index], merged);
+
+function mergeArray(array, merged = [], index = 0) {
+  if (index === array.length) return merged;
+  if (Array.isArray(array[index])) {
+    mergeArray(array[index], merged);
   } else {
-    merged.push(arr[index]);
+    merged.push(array[index]);
   }
-  return merge(arr, merged, index + 1);
+  return mergeArray(array, merged, index + 1);
 }
 
-merge(arr);
+console.log(mergeArray(arr));
--- 25-array/test.js
+++ 25-array/test.js
@@ -1,25 +1,27 @@
 function recur(n = 0) {
-  if (n >= 4) return n;
+  if (n >= 3) return n;
   return 2 + recur(n + 1);
 }
 console.log(recur());
 /* 
+!The value passing in recursion happens twice:
+1) First Pass (Bottom to Top): Values are passed forward as the stack builds.
+2) Second Pass (Top to Bottom): Values are passed backward as the stack unwinds.
 
-        recur(n+4)
-        recur(n+3)
-        recur(n+2)
-        recur(n+1)
-        recur(n=0) outermost call / outer call
------------------------------
-        call stack
+Call Stack:
+recur(3) → waits for nothing (as condition meets) "returns 3"
+recur(2) → waits for recur(3)
+recur(1) → waits for recur(2)
+recur(0) → waits for recur(1)
+--------------call stack (bottom to top) [1st pass]----------
+
+-------------unwind from call stack (Top to Bottom) [2nd pass]-------
+recur(3) → returns 3. pass "3" to down recur(recurtion: 3, value: 3)
+recur(2) → 2 + 3 = 5. pass "5" to down recur(recurtion: 2, value: 5)
+recur(1) → 2 + 5 = 7. pass "7" to down recur(recurtion: 1, value: 7)
+recur(0) → 2 + 7 = 9. no more pass and this "9" is the final value
 
 
-        no 2 as base case recur(n+0) = 0 outermost call/ outer call
-        2 + recur(n+1) = n is 1 now | this 1 will not be added to 2 but n is now 1
-        2 + recur(n+2) = n is 2 now | this 2 will not be added to 2 but n is now 2
-        2 + recur(n+3) = n is 3 now | this 3 will not be added to 2 but n is now 3
-        2 + recur(n+4) = n is finally 4 and only this 4 will be returned and added to 4 times 2 (2*2*2*2 + 4) 
------------------------------
-            rewind
+
 
 */
